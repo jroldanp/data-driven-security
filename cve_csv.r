5db31350e8cbf240eaa836d76efb4896c9bd3b44
jroldanp <75539916+jroldanp@users.noreply.github.com>
2020-12-24 00:07:20 +0100

    Update cve_csv.r

--- cve_csv.r
+++ cve_csv.r
@@ -1,4 +1,4 @@
-# This is to generate a wew temporary files to allocate feed data.
+# This is to generate a new temporary file to allocate feed data
 cves.src <- tempfile(fileext = ".csv")
 
 # To download content into temporary file
@@ -6,7 +6,8 @@ download.file(url = "https://cve.mitre.org/data/downloads/allitems.csv",
               destfile = cves.src)
 
 # Use of read.csv function from package readr. To have dataframe properly
-# formatted we skip 10 first rows where there is no tabulated data, we set # header as FALSE since there is no header with fileds and we define them
+# formatted we skip 10 first rows where there is no tabulated data, we set 
+# header as FALSE since there is no header with fileds and we define them
 # accordingly using col.names argument
 #
 cve <- read.csv(cves.src, header = FALSE,col.names = c("CVE Name","Status","Description","References","Phase","Votes","Comments"),skip = 10)
